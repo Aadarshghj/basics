a75895d498047bb748330e8c969aad59ee8a918b
aadarsh.ghj <aadarshaadarsh109@gmail.com>
2024-07-30 20:10:56 +0530

    first commit

---
+++ newscript.js
@@ -0,0 +1,101 @@
+/*console.log('My first code');
+// variables
+var a=5;
+console.log(a);
+let b=56;
+const n=67;
+{    
+   const n=90;
+    console.log(n);
+    var a=34;
+    console.log(a);
+    let b=34;
+    console.log(b);
+}
+var a=67;
+console.log(b);
+var firstName='Mri';
+console.log(typeof(firstName));
+let bool=true;
+console.log(typeof(bool));
+var arr1=['pinw','rose'];
+arr1.push('pine');
+console.log(arr1);
+//objects
+let person=new Object()
+   person.fname='Maya';
+     person.age=80;
+   // person.location: 'tvm';
+
+console.log(person)
+//let person=new Objects();
+//person.fname='Miya';
+//person.age=22;
+//console.log(person);
+//functions
+function display(fname){
+    console.log('Welcome'+fname+'to functions');
+}
+display('Aadarsh');//function call
+//to add
+function mul(a,b){
+   var sum=a*b;
+ return sum;
+}
+var result=mul(10,20);
+console.log(result);
+var ab=7;var abd=++ab;
+var abc=ab++;
+
+console.log(ab);
+console.log(abd);
+//console.log(abc);
+var exp=(4*7)-8**2+9;
+console.log(exp);
+
+//conditional statements
+if (exp>0) {
+   console.log("he vaslue is greaer than zero")
+   
+} else {
+   console.log('less than')
+   
+}
+
+//LOOPIN
+for (let i= 0; i<=10; ++i) {
+   console.log(i++);
+   
+}
+var array=[1,45,34,56,78];
+
+for (let i= 0; i<=10; ++i) {
+   console.log(array[i]);
+   
+}
+//for in 
+var obj={fname:'heera',age:34,place:'tvm'}                  
+for (const i in obj) {
+    console.log(obj[i]);
+   
+}
+//for of loop
+for (const i of array) {
+   console.log(i);
+   
+}
+//while
+let i=0;
+while (i<=10) {
+   console.log(i);
+   i++;
+}
+*/
+   // do while
+
+let r=1;
+do {
+   console.log(r);
+   r++;
+
+} while (r<=10);
